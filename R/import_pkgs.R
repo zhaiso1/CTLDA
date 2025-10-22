3832464404fe05159684e8b217e0312e134d9fd1
Song Zhai <95722940+zhaiso1@users.noreply.github.com>
2025-10-21 22:39:46 -0400

    Add files via upload

--- R/import_pkgs.R
+++ R/import_pkgs.R
@@ -5,7 +5,7 @@
 #' @import ACAT
 #' @import dplyr
 #' @import robustbase
-#' @importFrom aod wald.test2
+#' @importFrom aod wald.test
 #' @importFrom lme4 lmer
 #' @importFrom pracma linspace
 #' @importFrom nlme gnls corSymm gnlsControl varIdent lme lmeControl
